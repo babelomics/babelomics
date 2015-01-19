16487ef1b0eb07c066f4583c34ac8bda9599fcc6
frasator <frasator@gmail.com>
2015-01-19 19:21:06 +0100

    updated babelomics element

--- babelomics-web/build.sh
+++ babelomics-web/build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 mkdir -p build
-rm -rf build/index.html build/index.js build/workers build/fonts
+rm -rf build/index.html build/index.js build/workers build/fonts build/images
 
 vulcanize babelomics-index.html -o build/index.html --inline --strip --csp
 
 cp -r bower_components/fontawesome/fonts build/
 cp -r src/fonts/*.woff* build/fonts/
+cp -r src/images build/
 
 mkdir -p build/workers
 cp lib/jsorolla/src/lib/worker-fileupload.js build/workers/
@@ -14,3 +15,4 @@ sed -i s@WORKERS_PATH=\"lib/jsorolla/src/lib/\"@WORKERS_PATH=\"workers/\"@g buil
 
 sed -i s@../bower_components/fontawesome/fonts/fontawesome-webfont.@fonts/fontawesome-webfont.@g build/index.html
 sed -i s@../src/fonts/@fonts/@g build/index.html
+sed -i s@../src/images/@images/@g build/index.html
