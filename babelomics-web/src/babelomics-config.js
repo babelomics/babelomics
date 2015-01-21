c77fe21c622ba28925a0af7bcf0c96eb23624b93
Alejandro Alemán <alejandro.aleman.ramos@gmail.com>
2015-01-21 11:59:28 +0100

    Added configuration of genomeViewer

--- babelomics-web/src/babelomics-config.js
+++ babelomics-web/src/babelomics-config.js
@@ -20,3 +20,24 @@ if (
     OPENCGA_VERSION = "v1";
 //    OPENCGA_HOST = "http://localhost:8080/opencga/rest";
 }
+
+var AVAILABLE_SPECIES = {
+    "text": "Species",
+    "items": [
+        {
+            "text": "Vertebrates",
+            "items": [
+                {
+                    "text": "Homo sapiens",
+                    "assembly": "GRCh37.p10",
+                    "region": {"chromosome": "13", "start": 32889611, "end": 32889611},
+                    "chromosomes": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y", "MT"],
+                    "url": "ftp://ftp.ensembl.org/pub/release-75/"
+                },
+            ]
+        }
+    ]
+};
+
+/** Reference to a species from the list to be shown at start **/
+var DEFAULT_SPECIES = AVAILABLE_SPECIES.items[0].items[0];
